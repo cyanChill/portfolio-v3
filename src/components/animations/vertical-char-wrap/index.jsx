8d0ae65112d3eb8bcbfc2c6b59145f4ddb2281a2
cyanChill <anthonyliang9@gmail.com>
2022-06-28 12:25:57 -0400

    made component "generic"

--- src/components/animations/vertical-char-wrap/index.jsx
+++ src/components/animations/vertical-char-wrap/index.jsx
@@ -3,33 +3,36 @@
     - We input a string and when we hover over the string, each letter will
       translate up and then come from the bottom, returning the letter to
       it's original position
+    - We have 2 props:
+      1. text - The string that we'll apply the animation to
+      2. className - The classes applied to this component externally
 */
 
 import { v4 as uuidv4 } from "uuid";
 
 import "./index.css";
 
-const CharRiseLink = ({ href, text, className }) => {
+const VCharWrap = ({ text, className }) => {
   return (
-    <a href={href} className={`char-rise-container ${className}`}>
+    <div className={`vchar-wrap-container ${className}`}>
       {text.split("").map((char, charNum) => (
         <div className="letterContainer" key={uuidv4()}>
           <span
-            className="letter char-rise-start"
+            className="letter vchar-wrap-start"
             style={{ transitionDelay: `${charNum * 50}ms` }}
           >
             {char}
           </span>
           <span
-            className="letter char-rise-end"
+            className="letter vchar-wrap-end"
             style={{ transitionDelay: `${charNum * 50}ms` }}
           >
             {char}
           </span>
         </div>
       ))}
-    </a>
+    </div>
   );
 };
 
-export default CharRiseLink;
+export default VCharWrap;
