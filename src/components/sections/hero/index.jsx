8f26034a87ea1bb730dbce30e53dc8d8f99733b4
cyanChill <anthonyliang9@gmail.com>
2022-06-28 22:48:29 -0400

    general layout of hero section

--- src/components/sections/hero/index.jsx
+++ src/components/sections/hero/index.jsx
@@ -1,9 +1,35 @@
-import "./index.css";
+import { v4 as uuidv4 } from "uuid";
+
+import HeroSVGBackdrop from "../../svg-backdrops/hero-backdrop";
+import { WordRiseAnim } from "../../animations/word-rise";
+import styles from "./index.module.css";
+
+const Hero_Words = ["Anthony", "Liang", "FullStack", "Developer"];
 
 const Hero = () => {
   return (
-    <div id="hero">
-      <h1>Hero Section</h1>
+    <div id={styles.hero}>
+      <div className={styles.content}>
+        {Hero_Words.map((word, idx) => {
+          return (
+            <div className={styles[`padd-${idx}`]} key={uuidv4()}>
+              <WordRiseAnim
+                text={word}
+                delayMS={250 * (idx + 1)}
+                durationMS={450}
+              />
+              <br />
+            </div>
+          );
+        })}
+      </div>
+
+      <div className={styles.backdrop}>
+        <HeroSVGBackdrop
+          width="clamp(35rem, 100vw, 80rem)"
+          height="clamp(21.875rem, 62.5vw, 50rem)"
+        />
+      </div>
     </div>
   );
 };
