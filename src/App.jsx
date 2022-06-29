4f33bf3beac7e733fe080eee50b97edafe29f59e
cyanChill <anthonyliang9@gmail.com>
2022-06-29 16:46:48 -0400

    added wrapper to limit app width

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Hero from "./components/sections/hero";
 import About from "./components/sections/about";
 import Projects from "./components/sections/projects";
 import Contact from "./components/sections/contact";
+import styles from "./app.module.css";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
+      <div className={styles.wrapper}>
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </div>
     </>
   );
 }
